161c8c130cb04d848b32dd3160a8dd1e0e85390d
Test User <test@example.com>
2025-02-14 19:25:03 -0600

    Remove build directory from Git tracking

--- build/types.js
+++
@@ -1,9 +0,0 @@
-// Error types
-export class PersonalContextError extends Error {
-    code;
-    constructor(message, code) {
-        super(message);
-        this.code = code;
-        this.name = 'PersonalContextError';
-    }
-}
